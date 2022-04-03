9766c4682e11bcf49f00f06c7e1113d1445bfd3a
Fernanda <fernandasoares.reis1@gmail.com>
2022-04-03 16:21:29 -0300

    dia 3

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,21 +7,14 @@ public class Main {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 
-        String[] moviesArray = ParseJSON.parseJsonFilmes(API.api());
-        
-           
-        List<String> titles = ParseJSON.parseItem(moviesArray, 2);
-        titles.forEach(System.out::println);
+		String[] moviesArray = ParseJSON.parseJsonFilmes(API.api());
 
-        List<String> images = ParseJSON.parseItem(moviesArray, 5);
-        images.forEach(System.out::println);
+		List<Movie> movies = ParseJSON.listMovies(moviesArray);
         
-        List<String> ratings = ParseJSON.parseItem(moviesArray, 7);
-        ratings.forEach(System.out::println);
-        
-        List<String> years = ParseJSON.parseItem(moviesArray, 4);
-        years.forEach(System.out::println);
-;
+		for(int i = 0; i<movies.size();i++) {
+        	System.out.println(movies.get(i).getTitle());
+        }
+
 	}
        	
 
